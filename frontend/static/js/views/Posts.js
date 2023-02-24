15bfb5a6629b6f1c2fa5840c4fbdd0b0da5ecccc
JulieDeRen <julie.deschenes-renaud@hec.ca>
2023-02-24 18:39:47 -0500

    Optimiser la fonction search et server.js

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -50,79 +50,112 @@ export default class extends AbstractView {
 
     }
 
+    // fonction de recherche
     async search(e) {
-        console.log(this.#animal)
         async function getData(url) {
             console.log(url)
             const response = await fetch(url)
             return response.json()
         }
-        
-        if(this.#animal.includes("cat")){
+        // condition pour éviter que ex :  /cat soit /cat/
+        if (this.#animal.includes("cat")) {
             this.#animal = "/cat";
         }
-        else{
-            this.#animal ="/dog";
+        else {
+            this.#animal = "/dog";
         }
-        let animal = this.#animal;
-        console.log(animal)
+
+
+        // récupérer les données du fichier json et passer en paramètre de la fonction search dna la classe AbstractView
         const data = await getData('/static/js/views/data' + this.#animal + 'Data.json');
-        // envoyer le set de data en paramètre lorsque bouton click
         const abstractView = new AbstractView();
         let res = abstractView.search(data);
         const promise = Promise.resolve(res);
         promise.then((value) => {
             let datas = value;
             console.log(value)
+            this.cbSearch(datas)
+        })
+
+
+
+    }
+    // fonction de rappel pour afficher les données recherchées
+    cbSearch(datas) {
+        let animal = this.#animal;
+        // enlever le slash first char fonction crée dans AbstractView
+        let abstractView = new AbstractView;
+        animal = abstractView.typeAnimal(animal);
+
+
+        let post = `
+                <!-- ======= Portfolio Section Ref : BootstrapMade template Tempo======= -->
+                <section id="portfolio" class="portfolio">
+                  <div class="container">
             
-
-
-            let animal = this.#animal;
-            // enlever le slash first char
-            let abstractView = new AbstractView;
-            animal = abstractView.typeAnimal(animal);
+                    <div class="section-title">
+                      <h2>${animal}</h2>
+                    </div>
             
+                    <div class="row">
+                      <div class="col-lg-12 d-flex justify-content-center">
+                        <ul id="portfolio-flters">
+                          <li data-filter="*" class="filter-active">All</li>
+                          <li data-filter=".filter-app">Friendly</li>
+                          <li data-filter=".filter-card">Sportive</li>
+                          <li data-filter=".filter-web">Calm</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="row portfolio-container" style="position:relative">`
 
-            let post = `
-            <!-- ======= Portfolio Section Ref : BootstrapMade template Tempo======= -->
-            <section id="portfolio" class="portfolio">
-              <div class="container">
-        
-                <div class="section-title">
-                  <h2>${animal}</h2>
-                </div>
-        
-                <div class="row">
-                  <div class="col-lg-12 d-flex justify-content-center">
-                    <ul id="portfolio-flters">
-                      <li data-filter="*" class="filter-active">All</li>
-                      <li data-filter=".filter-app">Friendly</li>
-                      <li data-filter=".filter-card">Sportive</li>
-                      <li data-filter=".filter-web">Calm</li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="row portfolio-container" style="position:relative">`
-   
-           let img;
-           let url;
-           datas.forEach((uneData)=>{
-   
-               if (this.#animal.includes("cat")) {
-                   if (uneData.hasOwnProperty('reference_image_id')) {
-                       url = 'https://api.thecatapi.com/v1/images/' + uneData['reference_image_id'];
-                       console.log(url);
-                       fetch(url)
-                           .then((response) => response.json())
-                           .then((uneData) => {
-                               console.log('Success:', uneData);
-                               cb(uneData);
-                           })
-                           .catch((error) => {
-                               console.error('Error:', error);
-                           });
-                       post += `<div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position:relative">
-                                       <img src="${img}" class="img-fluid" alt="image carte" data-img="${uneData['reference_image_id']}">
+        let img;
+        let url;
+        datas.forEach((uneData) => {
+
+            if (this.#animal.includes("cat")) {
+                if (uneData.hasOwnProperty('reference_image_id')) {
+                    url = 'https://api.thecatapi.com/v1/images/' + uneData['reference_image_id'];
+                    console.log(url);
+                    fetch(url)
+                        .then((response) => response.json())
+                        .then((uneData) => {
+                            console.log('Success:', uneData);
+                            cb(uneData);
+                        })
+                        .catch((error) => {
+                            console.error('Error:', error);
+                        });
+                    post += `<div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position:relative">
+                                           <img src="${img}" class="img-fluid" alt="image carte" data-img="${uneData['reference_image_id']}">
+                                           <div class="portfolio-info" style="position:absolute">
+                                               <h4>${uneData['name']}</h4>
+                                               <p>${uneData['origin']}</p>
+                                               <a href="${this.#animal}/post-view/${uneData['id']}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="${uneData['name']}" data-link><i class="bx bx-plus"></i></a>
+                                               <a href="${this.#animal}/post-view/${uneData['id']}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                                           </div>
+                                       </div>`
+                }
+                function cb(data) {
+
+                    let img = document.querySelectorAll(".img-fluid");
+                    img.forEach((uneImg) => {
+
+                        if (uneImg.dataset.img == data.id) {
+                            uneImg.src = data.url;
+                        }
+                    })
+                }
+
+            }
+
+            else {
+
+                img = uneData['image']['url'];
+
+
+                post += `<div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position:relative">
+                                       <img src="${img}" class="img-fluid" alt="image carte">
                                        <div class="portfolio-info" style="position:absolute">
                                            <h4>${uneData['name']}</h4>
                                            <p>${uneData['origin']}</p>
@@ -130,51 +163,19 @@ export default class extends AbstractView {
                                            <a href="${this.#animal}/post-view/${uneData['id']}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                                        </div>
                                    </div>`
-                   }
-   
-               }
-   
-               else {
-   
-                   img = uneData['image']['url'];
-   
-   
-                   post += `<div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position:relative">
-                                   <img src="${img}" class="img-fluid" alt="image carte">
-                                   <div class="portfolio-info" style="position:absolute">
-                                       <h4>${uneData['name']}</h4>
-                                       <p>${uneData['origin']}</p>
-                                       <a href="${this.#animal}/post-view/${uneData['id']}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="${uneData['name']}" data-link><i class="bx bx-plus"></i></a>
-                                       <a href="${this.#animal}/post-view/${uneData['id']}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
-                                   </div>
-                               </div>`
-               };
-           })
-           post += `</div>
-                       </div>
-                       </section><!-- End Portfolio Section -->`;
-
-            const app = document.getElementById("app");
-
-            app.innerHTML = "";
-            console.log(app);
-            app.innerHTML = post;
-
+            };
         })
+        post += `</div>
+                           </div>
+                           </section><!-- End Portfolio Section -->`;
 
-        function cb(data) {
+        const app = document.getElementById("app");
 
-            let img = document.querySelectorAll(".img-fluid");
-            img.forEach((uneImg) => {
-    
-                if (uneImg.dataset.img == data.id) {
-                    uneImg.src = data.url;
-                }
-            })
-        }
-
-
+        app.innerHTML = "";
+        console.log(app);
+        app.innerHTML = post;
     }
+
 
     async getHtml() {
         async function getData(url) {
--- server.js
+++ server.js
@@ -5,120 +5,94 @@ const request = require('request');
 const config = require('./config.js')
 const { PORT } = require('./config');
 const { API_KEY } = require('./config');
-const app=express();
+const app = express();
 
 
 //app.get('/ticker=:id', function(req, res){ ...  ajout ... });
-function getDataCat(){
+function getDataCat() {
     let ticker = 'catData';
     let url = 'https://api.thecatapi.com/v1/breeds';
 
-        request.get({
-            url: url,
-            json: true,
-            headers: {'User-Agent': 'request'}
-        }, (err, res, data) => {
-            if (err) {
+    request.get({
+        url: url,
+        json: true,
+        headers: { 'User-Agent': 'request' }
+    }, (err, res, data) => {
+        if (err) {
             console.log('Error:', err);
-            } else if (res.statusCode !== 200) {
+        } else if (res.statusCode !== 200) {
             console.log('Status:', res.statusCode);
-            } else {
+        } else {
             // data is successfully parsed as a JSON object:
             let newData = JSON.stringify(data)
-                fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
-                if(err) throw err;
+            fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
+                if (err) throw err;
                 // console.log(newData);
                 console.log("success");
-                })
-                let datas = JSON.parse(newData);
+            })
 
-            }
-            
-        })
-    
+        }
+
+    })
+
 }
 
-function getCatImage(){
+function getCatImage() {
     let ticker = 'catImgData';
-    let url = 'https://api.thecatapi.com/v1/images/';
-    let newData='';
-  
-        fs.readFile(__dirname + "/frontend/static/js/views/data/catData.json", 'utf8', function (err, data){
-            data=>JSON.parse(data);
-            console.log(data);
-            JSON.parse(data).forEach((uneData)=>{
-                url += uneData.reference_image_id;
-                request.get({
-                    url: url,
-                    json: true,
-                    headers: {'User-Agent': 'request'}
-                }, (err, res, data) => {
-                    if (err) {
+    let newArray = [];
+    let newData = "";
+
+    fs.readFile(__dirname + "/frontend/static/js/views/data/catData.json", 'utf8', function (err, data) {
+        JSON.parse(data).forEach((uneData) => {
+            let url = 'https://api.thecatapi.com/v1/images/' + uneData.reference_image_id;
+            request.get({
+                url: url,
+                json: true,
+                headers: { 'User-Agent': 'request' }
+            }, (err, res, data) => {
+                if (err) {
                     console.log('Error:', err);
-                    } else if (res.statusCode !== 200) {
+                } else if (res.statusCode !== 200) {
                     console.log('Status:', res.statusCode);
-                    } else {
+                } else {
                     // data is successfully parsed as a JSON object:
                     newData += JSON.stringify(data)
-                    console.log(newData);
-                        fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
-                        if(err) throw err;
-                        // console.log(newData);
-                        console.log("success");
-                        })
-        
-                    }
-                    
+
+                }
+                fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
+                    if (err) throw err;
+                    // console.log(newData);
+                    console.log("success");
                 })
 
             })
+
         })
-        /*
-        request.get({
-            url: url,
-            json: true,
-            headers: {'User-Agent': 'request'}
-        }, (err, res, data) => {
-            if (err) {
-            console.log('Error:', err);
-            } else if (res.statusCode !== 200) {
-            console.log('Status:', res.statusCode);
-            } else {
-            // data is successfully parsed as a JSON object:
-            let newData = JSON.stringify(data)
-                fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
-                if(err) throw err;
-                // console.log(newData);
-                console.log("success");
-                })
-                let datas = JSON.parse(newData);
+    })
 
-            }
-            
-        }) */
 }
-getCatImage();
 
-function getDataDog(){
+
+function getDataDog() {
     let ticker = 'dogData';
     let url = 'https://api.thedogapi.com/v1/breeds';
 
     request.get({
         url: url,
         json: true,
-        headers: {'User-Agent': 'request'}
+        headers: { 'User-Agent': 'request' }
     }, (err, res, data) => {
         if (err) {
-        console.log('Error:', err);
+            console.log('Error:', err);
         } else if (res.statusCode !== 200) {
-        console.log('Status:', res.statusCode);
+            console.log('Status:', res.statusCode);
         } else {
-        // data is successfully parsed as a JSON object:
-        let newData = JSON.stringify(data)
+            // data is successfully parsed as a JSON object:
+            let newData = JSON.stringify(data)
             fs.writeFile('./frontend/static/js/views/data/' + ticker + '.json', newData, err => {
-            if(err) throw err;
-            // console.log(newData);
-            console.log("success");
+                if (err) throw err;
+                // console.log(newData);
+                console.log("success");
             })
         }
     })
@@ -126,17 +100,18 @@ function getDataDog(){
 // appel des fonctions fetch et store datas in file
 getDataCat();
 getDataDog();
+getCatImage();
 
 // créer une exception sur le dossier static
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 // toujours tomber sur index
 
-app.get('/*', function(req, res){
+app.get('/*', function (req, res) {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 
-app.listen(8082, ()=> console.log("server TP1 running..."));
+app.listen(8082, () => console.log("server TP1 running..."));
 
 
